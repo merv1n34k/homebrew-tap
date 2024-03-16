f2e6bbf8a5352b2742f45e19ea7c850222b0f41f
Oleksii Stroganov <merv1n@proton.me>
2024-03-16 03:28:26 +0200

    Changed sha256 for fastx_toolkit formulae

--- fastx_toolkit.rb
+++ fastx_toolkit.rb
@@ -4,7 +4,7 @@ class FastxToolkit < Formula
   license "AGPLv3"
   url "https://github.com/merv1n34k/fastx_toolkit/releases/download/v0.0.6/fastx_toolkit-0.0.6.tar.gz"
   head "https://github.com/merv1n34k/fastx_toolkit.git", branch: "homebrew"
-  sha256 "2f1fbdf1a84529cc2cba1037de320d55fa316a41c743b67c7eaf6db490ea2b66"
+  sha256 "6921e88dc2209ed8ec53e6a2edc0d5ec36c697ad19d7ecece543e723be618b29"
 
   depends_on "cmake" => :build
   depends_on "gcc" => :build
